5474bc33714528536ce044f009006b21fc2073c3
Hamza Benbrahim <benbrahim.hamza@proton.me>
2023-10-16 05:34:02 +0200

    feat: auto reload game on win

--- hitAction.php
+++ hitAction.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Entity\AbstractBee;
+
 require __DIR__ . '/vendor/autoload.php';
 
 session_start();
@@ -10,11 +12,13 @@ $target = rand(0, count($beePopulation) - 1);
 
 $beePopulation[$target]->takeDamage();
 
-if ($beePopulation[$target]->getLifePoints() == 0)
+if ($beePopulation[$target]->getLifePoints() == 0 && $beePopulation[0]->getBeeStatus() !== AbstractBee::DEAD)
 {
     array_splice($beePopulation, $target, 1);
+    $_SESSION["beePopulation"] = $beePopulation;
 }
-
-$_SESSION["beePopulation"] = $beePopulation;
-
 echo implode(" ", $beePopulation);
+
+if ($beePopulation[0]->getBeeStatus() === AbstractBee::DEAD) {
+    echo("<meta http-equiv='refresh' content='1'>");
+}
